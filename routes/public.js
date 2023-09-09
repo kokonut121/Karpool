0cafcfd7551c51a4a19d3b40f1eddb70f61f87eb
Patrick Feng <kokon@MacBook-Air.local>
2023-09-09 14:02:23 -0700

    header file

--- routes/public.js
+++ routes/public.js
@@ -4,5 +4,7 @@ module.exports = (app, mongo) => {
         res.render('../views/public/hompage.ejs');
     });
 
-    
+    app.get('/signin', (req, res) => {
+        res.render('../views/public/signin.ejs');
+    });
 }
